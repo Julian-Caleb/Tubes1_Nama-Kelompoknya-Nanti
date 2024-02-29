d0e08009cbc312d1d7f9cb08c2603047661150ea
Intermaze <rafikiprawhira@gmail.com>
2024-02-29 10:17:56 +0700

    feat: added prototype logic (greedyv1)

--- run-bots.bat
+++ run-bots.bat
@@ -1,5 +1,4 @@
 @echo off
-start cmd /c "python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo"
-start cmd /c "python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo"
+start cmd /c "python main.py --logic JustMoveBro --email=orange@email.com --name=justmove --password=123456 --team etimo"
+start cmd /c "python main.py --logic GreedyV1 --email=strongest@email.com --name=strongest --password=123456 --team etimo"
+
