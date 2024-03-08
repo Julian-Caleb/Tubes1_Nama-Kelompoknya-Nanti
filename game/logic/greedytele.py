d5dca1c3b3b6aa9be24b5a8aae9138ab1092216b
NameLess <daffakusumamulia@gmail.com>
2024-03-08 20:13:48 +0700

    feat: extra greedy on greedytele.py

--- game/logic/greedytele.py
+++ game/logic/greedytele.py
@@ -27,22 +27,47 @@ class GreedyTele(BaseLogic) :
 
     def findNearestDiamond(self, listDiamond: list[GameObject], listTeleporters: list[GameObject], bot: GameObject):
         nearestDistance = 0
+        rewardnya = 0
         nearestDiamond = Position(0,0)
         teleState = 0
         firstItem = True
         for diamond in listDiamond:
             if firstItem:
                 nearestDistance, teleState = self.nearestWithTele(diamond.position, bot.position, listTeleporters)
+                rewardnya = diamond.properties.points
+                nearestDiamond = diamond.position
+            else:
+                tempDistance, tempTeleState = self.nearestWithTele(diamond.position, bot.position, listTeleporters)
+                if (tempDistance) < (nearestDistance) or (abs(tempDistance-nearestDistance) <= 2 and diamond.properties.points > rewardnya) :
+                    nearestDistance = tempDistance
+                    nearestDiamond = diamond.position
+                    rewardnya = diamond.properties.points
+                    teleState = tempTeleState
+            firstItem = False
+        
+        return nearestDiamond, teleState, nearestDistance, rewardnya
+
+    def findNearestDiamondAlt(self, listDiamond: list[GameObject], listTeleporters: list[GameObject], bot: GameObject):
+        nearestDistance = 0
+        rewardnya = 0
+        nearestDiamond = Position(0,0)
+        teleState = 0
+        firstItem = True
+        for diamond in listDiamond:
+            if firstItem:
+                nearestDistance, teleState = self.nearestWithTele(diamond.position, bot.position, listTeleporters)
+                rewardnya = diamond.properties.points
                 nearestDiamond = diamond.position
             else:
                 tempDistance, tempTeleState = self.nearestWithTele(diamond.position, bot.position, listTeleporters)
                 if (tempDistance) < (nearestDistance):
                     nearestDistance = tempDistance
                     nearestDiamond = diamond.position
+                    rewardnya = diamond.properties.points
                     teleState = tempTeleState
             firstItem = False
         
-        return nearestDiamond, teleState
+        return nearestDiamond, teleState, nearestDistance, rewardnya
 
     def next_move(self, board_bot: GameObject, board: Board) :        
 
@@ -51,29 +76,45 @@ class GreedyTele(BaseLogic) :
         
         listDiamond = board.diamonds
         listTeleporters = [d for d in board.game_objects if d.type == "TeleportGameObject"]
+        listButton = [d for d in board.game_objects if d.type == "DiamondButtonGameObject"]
         if props.diamonds < 3 :
-            self.goal_position, teleState = self.findNearestDiamond(listDiamond, listTeleporters, board_bot)
+            self.goal_position, teleState, nearestDist, rewardnya = self.findNearestDiamond(listDiamond, listTeleporters, board_bot)
             if teleState == 1:
                 self.goal_position = listTeleporters[0].position
+                print(f"otw, poinnya {rewardnya}")
                 print(f"TeleporterA : ({self.goal_position.x}, {self.goal_position.y})")
             elif teleState == 2:
                 self.goal_position = listTeleporters[1].position
+                print(f"otw, poinnya {rewardnya}")
                 print(f"TeleporterB : ({self.goal_position.x}, {self.goal_position.y})")
             else: #teleState == 0:
+                print(f"waktumu sisa {board_bot.properties.milliseconds_left}")
+                print(f"otw, poinnya {rewardnya}")
                 print(f"Diamond : x = ({self.goal_position.x}, {self.goal_position.y})")
             
         else :
             base = board_bot.properties.base
-            _, teleState = self.nearestWithTele(base, board_bot.position, listTeleporters)
-            self.goal_position = base
-            if teleState == 1:
-                self.goal_position = listTeleporters[0].position
-                print(f"TeleporterA (Base) : ({self.goal_position.x}, {self.goal_position.y})")
-            elif teleState == 2:
-                self.goal_position = listTeleporters[1].position
-                print(f"TeleporterB (Base) : ({self.goal_position.x}, {self.goal_position.y})")
-            else: #teleState == 0:
-                print(f"Base : x = {self.goal_position.x}, y = {self.goal_position.y}")
+            _, teleState1 = self.nearestWithTele(base, board_bot.position, listTeleporters)
+            gp, teleState2, nearestDistDM, rewardnya = self.findNearestDiamondAlt(listDiamond, listTeleporters, board_bot)
+            if (nearestDistDM <= 2 and (props.diamonds+rewardnya)<=5):
+                if teleState2 == 1:
+                    self.goal_position = listTeleporters[0].position
+                elif teleState2 == 2:
+                    self.goal_position = listTeleporters[1].position
+                else: 
+                    self.goal_position = gp
+            else:
+                self.goal_position = base
+                if teleState1 == 1:
+                    self.goal_position = listTeleporters[0].position
+                    print(f"TeleporterA (Base) : ({self.goal_position.x}, {self.goal_position.y})")
+                elif teleState1 == 2:
+                    self.goal_position = listTeleporters[1].position
+                    print(f"TeleporterB (Base) : ({self.goal_position.x}, {self.goal_position.y})")
+                else: #teleState == 0:
+                    print(f"Base : x = {self.goal_position.x}, y = {self.goal_position.y}")
+
+            
             
         
         delta_x, delta_y = get_direction(
